3e2ac109e5180443bc971cd06ce3c187371a0d35
daicho <daichimax78@gmail.com>
2019-11-19 00:50:22 +0900

    ランキングの読み込みテスト

--- web/index.php
+++ web/index.php
@@ -1,11 +1,34 @@
 <?php
-	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
-		$uri = 'https://';
-	} else {
-		$uri = 'http://';
-	}
-	$uri .= $_SERVER['HTTP_HOST'];
-	header('Location: '.$uri.'/dashboard/');
-	exit;
+    // ディレクトリパス定義
+    $root_dir = "https://github.com/daicho/mahjong-club/raw/master/";
+    $system_dir = $root_dir . urlencode("成績管理システム") . "/";
+    $seiseki_dir = $system_dir . urlencode("成績") . "/";
+    
+    // ファイル読み込み
+    $path = "ランキング.csv"; //$seiseki_dir . urlencode("ランキング") . ".csv?" . date("YmdHis");
+    $temp_path = "temp/ランキング.csv";
+    $contents = file_get_contents($path);
+
+    if ($contents) {
+        // シフトJIS→UTF-8に変換
+        file_put_contents($temp_path, mb_convert_encoding($contents, "UTF-8", "SJIS"));
+
+        $csv = new SplFileObject($temp_path);
+        $csv->setFlags(SplFileObject::READ_CSV);
+
+        // 配列に格納
+        foreach ($csv as $row) {
+            if (!is_null($row[0]))
+                $data[] = $row;
+        }
+    }
 ?>
-Something is wrong with the XAMPP installation :-(
+
+<!-- テーブル表示 -->
+<?php foreach ($data as $row) { ?>
+    <tr>
+        <?php foreach ($row as $cell) { ?>
+            <td><?= $cell ?></td>
+        <?php } ?>
+    </tr>
+<?php } ?>
