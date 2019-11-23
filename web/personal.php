85c34543247cd4b1c08c548775b4322d213c6795
daicho <daichimax78@gmail.com>
2019-11-23 23:39:24 +0900

    個人成績ページの項目を追加

--- web/personal.php
+++ web/personal.php
@@ -26,12 +26,96 @@
 
     <h3>成績</h3>
     <table>
-        <?php for ($i = 1; $i < 32; $i++) { ?>
-            <tr>
-                <?php for ($j = 0; $j < 3; $j++) { ?>
-                    <td><?= $data[$i][$j] ?></td>
-                <?php } ?>
-            </tr>
-        <?php } ?>
+        <?php
+        for ($i = 1; $i <= 31; $i++) {
+            echo "<tr>";
+            for ($j = 0; $j <= 2; $j++) {
+                echo "<td>";
+                if ($j != 2 || $i < 23 || 28 < $i)
+                    echo $data[$i][$j];
+                echo "</td>";
+            }
+            echo "</tr>";
+        }
+        ?>
+    </table>
+
+    <h3>役</h3>
+    <table>
+        <?php
+        for ($i = 0; $i < count($data); $i++) {
+            if ($data[$i][4] == "") break;
+
+            echo "<tr>";
+            for ($j = 5; $j <= 8; $j++) {
+                echo $i == 0 ? "<th>" : "<td>";
+                echo $data[$i][$j];
+                echo $i == 0 ? "</th>" : "</td>";
+            }
+            echo "</tr>";
+        }
+        ?>
+    </table>
+
+    <h3>アガリ翻数</h3>
+    <table>
+        <?php
+        for ($i = 33; $i <= 46; $i++) {
+            echo "<tr>";
+            for ($j = 0; $j <= 2; $j++) {
+                echo $i == 33 ? "<th>" : "<td>";
+                echo $data[$i][$j];
+                echo $i == 33 ? "</th>" : "</td>";
+            }
+            echo "</tr>";
+        }
+        ?>
+    </table>
+
+    <h3>局別収支</h3>
+    <table>
+        <?php
+        for ($i = 0; $i <= 8; $i++) {
+            echo "<tr>";
+            for ($j = 13; $j <= 15; $j++) {
+                echo $i == 0 ? "<th>" : "<td>";
+                echo $data[$i][$j];
+                echo $i == 0 ? "</th>" : "</td>";
+            }
+            echo "</tr>";
+        }
+        ?>
+    </table>
+
+    <h3>開始位置別スコア</h3>
+    <table>
+        <?php
+        for ($i = 10; $i <= 14; $i++) {
+            echo "<tr>";
+            for ($j = 13; $j <= 15; $j++) {
+                echo $i == 10 ? "<th>" : "<td>";
+                echo $data[$i][$j];
+                echo $i == 10 ? "</th>" : "</td>";
+            }
+            echo "</tr>";
+        }
+        ?>
+    </table>
+
+    <h3>相性</h3>
+    <table>
+        <?php
+        for ($i = 16; $i <= count($data); $i++) {
+            if ($data[$i][13] == "") break;
+
+            echo "<tr>";
+            for ($j = 13; $j <= 15; $j++) {
+                echo $i == 16 ? "<th>" : "<td>";
+                echo $data[$i][$j];
+                echo $i == 16 ? "</th>" : "</td>";
+            }
+            echo "</tr>";
+        }
+        ?>
     </table>
 </body>
