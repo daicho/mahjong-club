60a233acbf25a06797adb3b37331927ddf922d95
Rikuto Ooike <fjsda526hdbs@gmail.com>
2019-11-23 16:54:53 +0900

    ランキング作成

--- web/index.php
+++ web/index.php
@@ -7,7 +7,7 @@
     $seiseki_dir = $system_dir . urlencode("成績") . "/";
     
     $fileReader = new FileReader($root_dir);
-    $data = $fileReader->loadCSV($seiseki_dir . urlencode("ランキング") . ".csv")
+    $data = $fileReader->loadCSV($seiseki_dir . urlencode("ランキング") . ".csv");
 ?>
 
 <!DOCTYPE html>
@@ -20,13 +20,23 @@
 <body>
     <h1>競技麻雀同好会 成績管理システム</h1>
     <h2>ランキング</h2>
-
+    
     <!-- テーブル表示 -->
-    <?php foreach ($data as $row) { ?>
-        <tr>
-            <?php foreach ($row as $cell) { ?>
-                <td><?= $cell ?></td>
+    <?php for($j = 1; $j < count($data[2]);$j +=2) { ?>
+        <h3><?= $data[0][$j]?></h3>
+        <table>
+            <tr>
+                <th>順位</th>
+                <th>個人名</th>
+            </tr>
+            <?php for($i = 2;$i < count($data) - 2;$i++){ ?>
+                <tr>
+                    <td><?= $data[$i][0] ?></td>
+                    <td><a href="personal.php?name=<?=$data[$i][$j]?>"><?= $data[$i][$j] ?></a></td>
+                    <td><?= $data[$i][$j + 1] ?></td>
+                </tr>
             <?php } ?>
-        </tr>
+        </table>
     <?php } ?>
+
 </body>
