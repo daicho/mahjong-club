cdd6a9b7bf8a4307426d4ce10c7f5230f1661538
daicho <daichimax78@gmail.com>
2019-11-23 22:39:49 +0900

    CSVの取得元を競技麻雀同好会のものに変更

--- web/graph.php
+++ web/graph.php
@@ -5,8 +5,8 @@
     $name = $_GET["name"];
 
     // ディレクトリパス定義
-    $root_dir = "https://github.com/daicho/mahjong/raw/master/";
-    $system_dir = urlencode("麻雀同好会3rd") . "/";
+    $root_dir = "https://github.com/daicho/mahjong-club/raw/master/";
+    $system_dir = urlencode("成績管理システム") . "/";
     $seiseki_dir = $system_dir . urlencode("成績") . "/";
     
     $fileReader = new FileReader($root_dir);
@@ -22,48 +22,38 @@
     <script src="Chart.js/Chart.min.js"></script>
 </head>
 <body>
-    <canvas id="myChart" width="100" height="100"></canvas>
-<script>
-var ctx = document.getElementById('myChart').getContext('2d');
-var myChart = new Chart(ctx, {
-    type:'line',
-    data: {
-        labels: [
-            <?php
-            for ($i = 1; $i < count($data); $i++) {
-                if ($data[$i][10] == "") break;
-                echo "'" . $data[$i][10] . "', ";
+    <canvas id="score" width="400" height="400"></canvas>
+    <script>
+        var ctx = document.getElementById("score").getContext("2d");
+        var myChart = new Chart(ctx, {
+            type: "line",
+            data: {
+                labels: [
+                    <?php
+                    for ($i = 1; $i < count($data); $i++) {
+                        if ($data[$i][10] == "") break;
+                        echo "'" . $data[$i][10] . "', ";
+                    }
+                    ?>
+                ],
+                datasets: [{
+                    label: "スコア",
+                    data: [
+                        <?php
+                        for ($i = 1; $i < count($data); $i++) {
+                            if ($data[$i][10] == "") break;
+                            echo str_replace("±", "", $data[$i][11]) . ", ";
+                        }
+                        ?>
+                    ],
+                    borderColor: "rgba(255, 99, 132, 1)",
+                    borderWidth: 4,
+                    lineTension: 0,
+                    fill: false,
+                    pointBackgroundColor: "rgba(0, 0, 0, 0)",
+                    pointBorderColor: "rgba(0, 0, 0, 0)"
+                }]
             }
-            ?>
-        ],
-        datasets: [{
-            label: '# of Votes',
-            data: [
-                <?php
-                for ($i = 1; $i < count($data); $i++) {
-                    if ($data[$i][10] == "") break;
-                    echo str_replace("±", "", $data[$i][11]) . ", ";
-                }
-                ?>
-            ],
-            backgroundColor: 'rgba(0, 0, 0, 0)',
-            borderColor: 'rgba(255, 99, 132, 1)',
-            lineTension: 0,
-            borderWidth: 4
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        }
-    }
-});
-</script>
-    <h1>競技麻雀同好会 成績管理システム</h1>
-    <h2>ランキング</h2>
-
+        });
+    </script>
 </body>
