dd3926d5e79dd574ad3c945b32613d4d2d944e41
daicho <daichimax78@gmail.com>
2019-11-24 11:58:31 +0900

    .htaccessを追加

--- web/ranking.php
+++ web/ranking.php
@@ -5,7 +5,7 @@
     $root_dir = "https://github.com/daicho/mahjong-club/raw/master/";
     $system_dir = urlencode("成績管理システム") . "/";
     $seiseki_dir = $system_dir . urlencode("成績") . "/";
-    
+
     $fileReader = new FileReader($root_dir);
     $data = $fileReader->loadCSV($seiseki_dir . urlencode("ランキング") . ".csv");
 ?>
@@ -22,17 +22,17 @@
     <h2>ランキング</h2>
     
     <!-- テーブル表示 -->
-    <?php for($j = 1; $j < count($data[2]);$j +=2) { ?>
-        <h3><?= $data[0][$j]?></h3>
+    <?php for ($j = 1; $j < count($data[2]);$j +=2) { ?>
+        <h3><?= $data[0][$j] ?></h3>
         <table>
             <tr>
                 <th>順位</th>
                 <th>個人名</th>
             </tr>
-            <?php for($i = 2;$i < count($data) - 2;$i++){ ?>
+            <?php for ($i = 2; $i < count($data) - 2; $i++) { ?>
                 <tr>
                     <td><?= $data[$i][0] ?></td>
-                    <td><a href="personal.php?name=<?=$data[$i][$j]?>"><?= $data[$i][$j] ?></a></td>
+                    <td><a href="personal/<?= $data[$i][$j] ?>"><?= $data[$i][$j] ?></a></td>
                     <td><?= $data[$i][$j + 1] ?></td>
                 </tr>
             <?php } ?>
