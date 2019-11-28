71bd214fa288af0e035c49302278997ea2724719
Rikuto Ooike <fjsda526hdbs@gmail.com>
2019-11-28 20:31:18 +0900

    グラフを表示

--- web/graph.php
+++ web/graph.php
@@ -22,10 +22,21 @@
     <script src="Chart.js/Chart.min.js"></script>
 </head>
 <body>
-    <canvas id="score" width="400" height="400"></canvas>
+    <div style="width:50%;">
+        <h1>スコア</h1>
+        <canvas id="score" ></canvas>
+    </div>
+    <div style="width:50%;">
+        <h1>局別スコア</h1>
+        <canvas id="byStationScore"></canvas>
+    </div>
+    <div style="width:50%;">
+        <h1>あがり翻数</h1>
+        <canvas id="fanScore"></canvas>
+    </div>
     <script>
         var ctx = document.getElementById("score").getContext("2d");
-        var myChart = new Chart(ctx, {
+        var myScore = new Chart(ctx, {
             type: "line",
             data: {
                 labels: [
@@ -53,6 +64,70 @@
                     pointBackgroundColor: "rgba(0, 0, 0, 0)",
                     pointBorderColor: "rgba(0, 0, 0, 0)"
                 }]
+            }
+        });
+    
+        let ctxByStation = document.getElementById('byStationScore').getContext('2d');
+        let myScoreByStation  = new Chart(ctxByStation, {
+            type: 'line',
+            data:{
+                labels: [
+                    <?php
+                    for ($i = 1; $i < count($data); $i++) {
+                        if ($data[$i][13] == "") break;
+                        echo "'" . $data[$i][13] . "', ";
+                    }
+                    ?>
+                ],
+                datasets: [{
+                    label: "局別スコア",
+                    data: [
+                        <?php
+                        for ($i = 1; $i < count($data); $i++) {
+                            if ($data[$i][13] == "") break;
+                            echo str_replace("±", "", $data[$i][14]) . ", ";
+                        }
+                        ?>
+                    ],
+                    borderColor: "rgba(255, 99, 132, 1)",
+                    borderWidth: 4,
+                    lineTension: 0,
+                    fill: false,
+                    pointBackgroundColor: "rgba(0, 0, 0, 0)",
+                    pointBorderColor: "rgba(0, 0, 0, 0)"
+                }]
+
+            }
+        });
+
+
+        let ctxfan = document.getElementById('fanScore').getContext('2d');
+        let myFanScore  = new Chart(ctxfan, {
+            type: 'bar',
+            data:{
+                labels: [
+                    <?php
+                    for ($i = 34; $i < count($data); $i++) {
+                        if ($data[$i][0] == "") break;
+                        echo "'" . $data[$i][0] . "', ";
+                    }
+                    ?>
+                ],
+                datasets: [{
+                    label: "あがり翻数",
+                    data: [
+                        <?php
+                        for ($i = 34; $i < count($data); $i++) {
+                            if ($data[$i][0] == "") break;
+                            echo str_replace("回", "", $data[$i][1]) . ", ";
+                        }
+                        ?>
+                    ],
+                    borderColor: "rgba(255, 99, 132, 1)",
+                    backgroundColor:"rgba(255,99,132,0.3)",
+                    borderWidth: 4,
+                }]
+
             }
         });
     </script>
