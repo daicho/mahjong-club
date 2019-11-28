ddde9e0690a60906787146b6ca599e8220ac4cd1
Rikuto Ooike <fjsda526hdbs@gmail.com>
2019-11-28 20:49:52 +0900

    グラフを公開できるようにした

--- web/person.php
+++ web/person.php
@@ -19,7 +19,7 @@
     <meta charset="UTF-8">
     <title>成績管理システム</title>
     <link rel="stylesheet" href="style.css">
-    <script src="Chart.js/Chart.min.js"></script>
+    <script src="/Chart.js/Chart.min.js"></script>
 </head>
 <body>
     <h1>競技麻雀同好会 成績管理システム</h1>
