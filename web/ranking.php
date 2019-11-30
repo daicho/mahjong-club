1d20ff75bd6707fd6b45f028418e2c49f8bce467
kumokumo1653 <fjsda526hdbs@gmail.com>
2019-11-30 19:15:06 +0900

    css追加

--- web/ranking.php
+++ web/ranking.php
@@ -16,41 +16,45 @@
 <head>
     <meta charset="UTF-8">
     <title>成績管理システム</title>
-    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="/css/ranking.css">
 </head>
 <body>
     <h1>競技麻雀同好会 成績管理システム</h1>
     <h2>ランキング</h2>
     
     <!-- テーブル表示 -->
-    <?php for ($j = 1; $j < count($data[2]);$j +=2) { ?>
-        <h3><?= $data[0][$j] ?></h3>
-        <table>
-            <tr>
-                <th>順位</th>
-                <th>個人名</th>
-            </tr>
-            <?php 
-            $rank = 1;
-            $disprank = 1;
-            ?>
-            <?php for ($i = 2; $i < count($data); $i++) { ?>
-                <tr>
-                    <?php if($data[$i][$j + 1] != $data[$i - 1][$j + 1]){ 
-                        $disprank = $rank;
-                    }
-                    if("全体" != $data[$i][$j]){
-                        $rank++;
+    <div class='ranking-container'>
+        <?php for ($j = 1; $j < count($data[2]);$j +=2) { ?>
+            <div class='table'>
+                <h3><?= $data[0][$j] ?></h3>
+                <table>
+                    <tr>
+                        <th>順位</th>
+                        <th>個人名</th>
+                    </tr>
+                    <?php 
+                    $rank = 1;
+                    $disprank = 1;
                     ?>
-                        <td><?=  $disprank . "位"?></td>
-                    <?php }else {?>
-                        <td></td>
+                    <?php for ($i = 2; $i < count($data); $i++) { ?>
+                        <tr>
+                            <?php if($data[$i][$j + 1] != $data[$i - 1][$j + 1]){ 
+                                $disprank = $rank;
+                            }
+                            if("全体" != $data[$i][$j]){
+                                $rank++;
+                            ?>
+                                <td><?=  $disprank . "位"?></td>
+                            <?php }else {?>
+                                <td></td>
+                            <?php } ?>
+                            <td><a href="personal/<?= $data[$i][$j] ?>"><?= $data[$i][$j] ?></a></td>
+                            <td><?= $data[$i][$j + 1] ?></td>
+                        </tr>
                     <?php } ?>
-                    <td><a href="personal/<?= $data[$i][$j] ?>"><?= $data[$i][$j] ?></a></td>
-                    <td><?= $data[$i][$j + 1] ?></td>
-                </tr>
-            <?php } ?>
-        </table>
-    <?php } ?>
+                </table>
+            </div>
+        <?php } ?>
+    </div>
 
 </body>
