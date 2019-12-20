486fa682d9c5c822cb0c2493c0f02bcf5df69b95
fukuzawa <16426@g.nagano-nct.ac.jp>
2019-12-20 19:29:22 +0900

    個人成績が見れないバグを修正

--- LINE@/mahjong-club.php
+++ LINE@/mahjong-club.php
@@ -194,6 +194,7 @@ if ($event_type == "follow" || $event_type == "join") {
         $fname = $record . urlencode($name) . ".csv?" . date("YmdHis");
         $graph_score = $record . urlencode($message_text) . "-Score.png?" . date("YmdHis");
         $graph_kyoku = $record . urlencode($message_text) . "-Kyoku.png?" . date("YmdHis");
+        $graph_fan = $record . urlencode($message_text) . "-Fan.png?" . date("YmdHis");
         $myfname = "record/" . $message_text . ".csv";
 
         // 名前が存在したら
