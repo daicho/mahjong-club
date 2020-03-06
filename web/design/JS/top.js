924ef174c06dc7ac615f6489b4a1a7cac25813e3
角田創 <HAZI@tsunodahajime-no-MacBook-Air.local>
2020-03-06 18:48:47 +0900

    「参加者」の項目を追加

--- web/design/JS/top.js
+++ web/design/JS/top.js
@@ -1,12 +1,39 @@
-var RankImg = new Array("svg/rank_frame.svg", "svg/rank_fill.svg");
-var ManImg = new Array("svg/man_frame.svg", "svg/man_fill.svg");
+const RankImg = new Array("svg/rank_frame.svg", "svg/rank_fill.svg");
+const ManImg = new Array("svg/man_frame.svg", "svg/man_fill.svg");
 
-function man_click(){
-    document.getElementById("man_img").src = ManImg[1];
-    document.getElementById("rank_img").src = RankImg[0];
+let rank_block = document.getElementById("rank_block");
+let member_block = document.getElementById("member_block");
+
+const rankClick = () => {
+    document.getElementById("man_img").src = ManImg[0];
+    document.getElementById("rank_img").src = RankImg[1];
+
+    member_block.style.display = "none";
+    rank_block.style.display = "block";
 }
 
-function rank_click(){
-    document.getElementById("man_img").src=ManImg[0];
-    document.getElementById("rank_img").src=RankImg[1];
+const manClick = () => {
+    document.getElementById("man_img").src = ManImg[1];
+    document.getElementById("rank_img").src = RankImg[0];
+
+    member_block.style.display = "block";
+    rank_block.style.display = "none";
+
+    checkMember();
+}
+
+const checkMember = () => {
+    let number = document.getElementsByClassName("number");
+    let member_num = number.length;
+
+    let count = 0;
+    
+    for(let i = 0; i < member_num; i++)
+    {
+        count += 1;
+
+        if(i == 9) count = 1;
+
+        number[i].src = "svg/" + count + ".svg"; 
+    }
 }
