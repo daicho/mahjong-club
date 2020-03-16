06c484b4bbfab3fd40cc2cffe2199b9a7a687326
kumokumo1653 <fjsda526hdbs@gmail.com>
2020-03-16 20:25:45 +0900

    ヒストグラムのグラデーションを調整

--- web/js/graph.js
+++ web/js/graph.js
@@ -83,10 +83,14 @@ const viewFanGraph = () => {
     let ctxFan = document.getElementById("fan_graph").getContext("2d");
 
     // グラデーション作成
-    let height = window.innerHeight || document.body.clientHeight;
-    let gradientStroke = ctxFan.createLinearGradient(0, 0, 0, height);
-    gradientStroke.addColorStop(0, "rgb(226, 199, 85)");
-    gradientStroke.addColorStop(0.5, "rgba(0, 0, 0, 0)");
+    let height = document.getElementById("win_figure").clientHeight ;
+    let gradientStrokes = new Array(13);
+    maxFantimes = dataFan.reduce((a,b) => a > b ? a : b);
+    for(let i = 0; i < gradientStrokes.length; i++){
+        gradientStrokes[i] = ctxFan.createLinearGradient( 0, height, 0 , height - height * (dataFan[i] / maxFantimes));
+        gradientStrokes[i].addColorStop(1, "rgb(226, 199, 85)");
+        gradientStrokes[i].addColorStop(0, "rgba(0, 0, 0, 0)");
+    }
 
     let chartFan = new Chart(ctxFan, {
         type: "bar",
@@ -96,7 +100,7 @@ const viewFanGraph = () => {
                 label: "出現回数",
                 data: dataFan,
                 borderColor: "rgb(226, 199, 85)",
-                backgroundColor: gradientStroke,
+                backgroundColor: gradientStrokes,
                 borderWidth: 2,
                 categoryPercentage: 1.2
             }]
